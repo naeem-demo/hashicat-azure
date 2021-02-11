1ac1a5a6b6ffcc7702e26aded9c4a06b99baf7c3
naeem-demo <78924155+naeem-demo@users.noreply.github.com>
2021-02-11 12:50:54 -0500

    Remove quotes around True

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "azurerm_resource_group" "myresourcegroup" {
   tags = {
     environment = "Production",
     Department  = "devops",
-    Billable    = "true"
+    Billable    = True
   }
 }
 
